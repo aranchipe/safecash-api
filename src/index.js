e0fabddeb0164600f174e4fc8f63faa7bf78a852
Lucas Aranchipe <lucas.aranchipe@geo-pointer.com>
2023-06-21 20:47:03 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const rotas = require("./rotas");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Configurar a origem permitida para acessar a API
+  })
+);
 app.use(rotas);
 app.listen(process.env.PORT || 3334);
