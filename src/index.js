3f3f73aee1f72cffb62ebfd3c69ddf83c30a37ab
Lucas Aranchipe <lucas.aranchipe@geo-pointer.com>
2023-06-21 23:18:20 -0300

    vercel file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 require("dotenv").config();
+const postgres = require("postgres");
 const cors = require("cors");
 const express = require("express");
 /* const { createProxyMiddleware } = require("http-proxy-middleware"); */
 const rotas = require("./rotas");
 const app = express();
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const sql = postgres(URL, { ssl: "require" });
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
 app.use(express.json());
 app.use(cors());
 /* app.use(
@@ -19,17 +31,5 @@ app.use(cors());
   })
 ); */
 app.use(rotas);
-const postgres = require("postgres");
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
-const sql = postgres(URL, { ssl: "require" });
-
-async function getPgVersion() {
-  const result = await sql`select version()`;
-  console.log(result);
-}
-
-getPgVersion();
 app.listen(process.env.PORT || 3334);
