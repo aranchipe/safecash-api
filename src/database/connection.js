417ed280747e35cc5fbdde4b941d5d74e3a0622f
Lucas Aranchipe <lucas.aranchipe@geo-pointer.com>
2023-06-21 23:37:12 -0300

    vercel file

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,10 +5,10 @@ const knex = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-
-    /* ssl: {
+    ssl: {
       rejectUnauthorized: false,
-    }, */
+    },
+    sslmode: "require",
   },
 });
 
